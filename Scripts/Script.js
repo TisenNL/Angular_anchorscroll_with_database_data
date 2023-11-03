4b06f443fb2366bd5508c5198fbd481258072288
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-11-03 10:23:47 -0300

    first commit

---
+++ Scripts/Script.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.js" />
+
+var demoApp = angular.module("demoApp", [])
+                     .controller("countryController", function ($scope, $http, $location, $anchorScroll) {
+                         $http.get("CountryService.asmx/GetData")
+                              .then(function (response) {
+                                  $scope.contries = response.data;
+                              });
+
+                         $scope.scrollTo = function (countryName) {
+                             $location.hash(countryName);
+                             $anchorScroll();
+                         }
+                     });
